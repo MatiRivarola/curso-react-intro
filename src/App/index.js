953e2529c89332a6ab7a695a6e8383f51e163a01
MatiRivarola <mati.rivarola15@gmail.com>
2023-06-15 14:11:57 -0300

    organizamos carpetas

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { TodoCounter } from './TodoCounter';
-import { TodoCreate} from './TodoCreate';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
-import { CancelButton } from './CreateTodoButton';
+import { TodoCounter } from '../TodoCounter/index';
+import { TodoCreate} from '../TodoCounter/TodoCreate';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoIcon/TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { CancelButton } from '../CreateTodoButton';
 import './App.css'
-import { TodoSearch } from './Todosearch';
-
+import { TodoSearch } from '../Todosearch';
+import { useLocalStorage } from "./useLocalStorage";
 
 // const defaultTask = [
 //   { text: 'Organizar cuarto', completed: true },
@@ -17,30 +17,6 @@ import { TodoSearch } from './Todosearch';
 
 // localStorage.setItem('TODOS_V1',JSON.stringify(defaultTask))
 
-
-function useLocalStorage(itemName , initialValue) {
-
-  const localStorageItem = localStorage.getItem(itemName)
-  let parsedItem;
-
-  if (!localStorageItem) {
-
-    localStorage.setItem(itemName, JSON.stringify([initialValue]))
-    parsedItem = initialValue
-  }else{
-    parsedItem = JSON.parse(localStorageItem)
-  }
-  
-  const [item , setItem] = React.useState(parsedItem);
-  
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-
-  };
-
-  return [item , saveItem];
-}
 
 
 function App() {
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,4 +1,4 @@
-import "./App.css"
+import "../App/App.css"
 
 function TodoList( { children } ) {
     return(
